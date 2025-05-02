813decb3b4d01b423bcd2f378ae34b247aae15dc
Юля <@DESKTOP-LDN2BMM>
2025-05-02 23:29:24 +0300

    Pointers Encoding

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -6,9 +6,9 @@ void main()
 	setlocale(LC_ALL, "");
 	cout << "Hello Pointers" << endl;
 	int a = 2;
-	int* pa = &a;
-	cout << a << endl;
-	cout << &a << endl;
-	cout << pa << endl;
-	cout << *pa << endl;
+	int* pa = &a; //объявление указателя
+	cout << a << endl;  //вывод переменной на экран
+	cout << &a << endl; //амперсант - взятие адреса переменной а при выводе
+	cout << pa << endl; //вывод адреса пременной а, хранящегося в указателе ра
+	cout << *pa << endl;//взятие значения а из указателя
 }
