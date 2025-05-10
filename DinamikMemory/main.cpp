f8ddebfdf45b3c4ebd35a34194a34b2ebb07070c
Юля <@DESKTOP-LDN2BMM>
2025-05-10 11:00:04 +0300

    DinamicMemory1DONE

--- DinamikMemory/main.cpp
+++ DinamikMemory/main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
 int* Push_back(int arr[], int& n, const int value);
-int* Erase(int arr[], int& w, const int n);
+int* Push_front(int arr[], int& n, const int value);
+int* insert(int arr[], int& n, const int value, int index);
+int* Pop_back(int arr[], int& n);
 
 void main()
 {
@@ -29,17 +31,21 @@ void main()
 	cout << "Введите добавляемое значение"; cin >> value;
 	arr = Push_back(arr, n, value);
 	Print(arr, n);
-
-	delete[] arr;
-	//Memory leak
-
 	
-	int w;
-	cout << "Введите номер элемента который нужно удалить"; cin >> w;
-	arr = Erase(arr, n, w);
+	cout << "Введите добавляемое значение"; cin >> value;
+	arr = Push_front(arr, n, value);
 	Print(arr, n);
 	
+	int index;
+	cout << "Введите индекс добавляемого значение"; cin >> index;
+	cout << "Введите значение добавляемого значение"; cin >> value;
+	arr = insert(arr, n, value, index);
+	Print(arr, n);
+
+	Print(arr = Pop_back(arr, n), n);// удален последний элемент массива
+
 	delete[] arr;
+
 }
 
 void FillRand(int arr[], const int n)
@@ -84,21 +90,38 @@ int* Push_back(int arr[], int& n, const int value)
 	
 	return arr;
 }
-int* Erase(int arr[], int& w, const int n)
+int* Push_front(int arr[], int& n, const int value)
 {
-	int* buffer = new int[n - 1];
+	int* buffer = new int[n + 1];
 	for (int i = 0; i < n; i++)
 	{
-		buffer[i] = arr[i];
+		buffer[i + 1] = arr[i];
 	}
-	cout << endl;
-
+	delete arr;
+	buffer[0] = value;
+	n++;
+	return buffer;
+}
+int* insert(int arr[], int& n, const int value, int index)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < index; i++)
+		buffer[i] = arr[i];
+	
+	for (int i = index; i < n; i++)
+		buffer[i + 1] = arr[i];
+	
 	delete[] arr;
-	arr = buffer;
-	arr[n] = w;
-	
-	
 
-	return arr;
-	
+	buffer[index] = value;
+	n++;
+	return buffer;
+}
+int* Pop_back(int arr[], int& n)
+{
+	cout << arr << endl;
+	int* buffer = new int[--n];//памать выделится в первую очередь из-за --
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];//копируем массив в буффер
+	delete[] arr;//удаляем исходный
+	return buffer;
 }
